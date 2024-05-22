8c788999297f0f47a466b5eaa1eb7586e2563b09
Eduardo Cortez Valente <130951926+EduardoCorVal@users.noreply.github.com>
2024-05-21 20:32:17 -0600

    Nutshell

---
+++ main.py
@@ -0,0 +1,6 @@
+from similarityCalculation import similarityCalculation
+
+if __name__ == '__main__':
+    plagiarism = similarityCalculation()
+    
+    plagiarism.dataBaseProcessing()
---
+++ similarityCalculation.py
@@ -0,0 +1,24 @@
+import ssl
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+class similarityCalculation():
+    
+    def __init__(self) -> None:
+        pass
+    
+    def preProcessing(self):
+        pass
+
+    def dataBaseProcessing(self):
+        print('Hello')
+    
+    def similarityComparison(self):
+        pass
+    
+    def _uploadDatabase(self):
+        pass
